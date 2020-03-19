79bf4749c4e816c9942ce1fde44a83de95d4f7c4
Srilal Sachintha <37685396+SrilalS@users.noreply.github.com>
2020-03-19 16:16:58 +0530

    mxi

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,41 @@
 import 'package:flutter/material.dart';
+import 'package:onesignal_flutter/onesignal_flutter.dart';
 
-void main() => runApp(MyApp());
+
+
+void main() => runApp(MyApp(
+));
 
 class MyApp extends StatelessWidget {
+  
   @override
+  
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+
+      title: 'FOSS NSBM',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'FOSS NSBM App'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
+  
   MyHomePage({Key key, this.title}) : super(key: key);
-
   final String title;
-
+  
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
+
+
 class _MyHomePageState extends State<MyHomePage> {
+  
+
   int _counter = 0;
 
   void _incrementCounter() {
@@ -35,28 +46,19 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
+    OneSignal.shared.init("df0e685a-2818-43f7-8ecf-a873b48aa4f3");
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text(
-              'You have pushed the button this many times:',
+              'Hello!',
+              style: TextStyle(fontSize:32),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
+            
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
       ),
     );
   }
